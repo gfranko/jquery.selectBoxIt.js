73b576a0891b5eedfde68c274b14c973d0ddb108
Greg Franko <gfranko5@yahoo.com>
2012-04-09 22:33:36 -0400

    testing new flip logic

--- javascripts/jQuery.selectBoxIt.js
+++ javascripts/jQuery.selectBoxIt.js
@@ -223,15 +223,15 @@
             listHeight = self.list.height(),
             //The height of the dropdown list DOM element
             selectBoxHeight = self.div.height();
-            //Places the dropdown list options list on top of the dropdown list if the dropdown list options list
-            //(when opened) does not fit on the page.
+            //Places the dropdown list options list on top of the dropdown list if the dropdown list options list does not fit on the page when opened
             if((listOffsetTop + selectBoxHeight + listHeight >= $(window).height() + $(window).scrollTop()) && (listOffsetTop - listHeight >= 0)){
-                //If the dropdown list already opens upward
+                //If the dropdown list currently opens downward
                 if(!self.flipped) {
                     //Inserts dropdown list options into the DOM before the dropdown list
-                    self.list.insertBefore(self.div).
+                    self.list.css("top", (self.divContainer.position().top - self.list.height()) - 2);
+                    //self.list.insertBefore(self.div).
                     //Sets custom CSS properties to place the dropdown list options directly above the dropdown list
-                    css({ "margin-top": -listHeight - 2 });
+                    //css({ "margin-top": -listHeight - 2 });
                     //Set the `flipped` instance variable to true to reflect that the dropdown list opens upward
                     self.flipped = true;
                 }
@@ -241,9 +241,10 @@
                 //If the dropdown list is currently opening upward
                 if(self.flipped) {
                     //Move the dropdown list list after the dropdown list div in the DOM
-                    self.list.insertAfter(self.div).
+                    //self.list.insertAfter(self.div).
                     //Dynamically defaults the margin-top back to 0
-                    css({"margin-top": "0px" });
+                    //css({"margin-top": "0px" });
+                    self.list.css("top", (self.divContainer.position().top + self.div.height()) + 2);
                     //Sets the `flipped` instance variable to false to reflect that the dropdown list opens downward
                     self.flipped = false;
                 }

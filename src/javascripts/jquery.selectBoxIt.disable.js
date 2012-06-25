ab21201347e9fac2e1f39888d7f5405d253d3660
Greg Franko <gfranko5@yahoo.com>
2012-06-25 12:09:38 -0400

    Modularized SelectBoxIt.  Most API Methods are now within their own files

---
+++ src/javascripts/jquery.selectBoxIt.disable.js
@@ -0,0 +1,55 @@
+$(function() {
+
+    //Disable
+    // ------
+    //      Disables the new dropdown list
+
+    $.selectBox.selectBoxIt.prototype.disable = function(callback) {
+
+            if(!this.options.disabled) {
+
+                //Makes sure the dropdown list is closed
+                this.close();
+
+                //Triggers a `disable` custom event on the original select box
+                this.selectBox.trigger("disable")
+
+                //Sets the `disabled` attribute on the original select box
+                .attr("disabled", "disabled");
+
+                //Makes the dropdown list not focusable by removing the `tabindex` attribute
+                this.div.removeAttr("tabindex").css("cursor", "default");
+
+                // Calls the jQueryUI Widget Factory disable method to make sure all options are correctly synced
+                $.Widget.prototype.disable.call(this);
+
+                //Provides callback function support
+                this._callbackSupport(callback);
+
+                //Maintains chainability
+                return this;
+
+            }
+
+    };
+
+    //_Is Disabled
+    // -----------
+    //      Checks the original select box for the
+    //    disabled attribute
+
+    $.selectBox.selectBoxIt.prototype._isDisabled = function(callback) {
+
+        //If the original select box is disabled
+        if (this.originalElem.disabled) {
+
+	        //Disables the dropdown list
+            this.disable();
+        }
+
+        //Maintains chainability
+        return this;
+
+    };
+
+});

ef87c6d6a01a878dae5036209c67a56abe6f3f5b
Greg Franko <gregfranko5@gmail.com>
2012-09-26 03:42:11 -0400

    Fixing search bug

--- src/javascripts/jquery.selectBoxIt.keyboardSearch.js
+++ src/javascripts/jquery.selectBoxIt.keyboardSearch.js
@@ -89,7 +89,7 @@ $(function() {
             }
 
             //Searches based on the first letter of the dropdown list options text if the currentText < 2 characters
-            if (this.currentText.length < 2) {
+            if (this.currentText.length < 3) {
 
                 //If there is a match based on the first character
                 if ((this.textArray[x].charAt(0).search(alphaNumeric) !== -1)) {

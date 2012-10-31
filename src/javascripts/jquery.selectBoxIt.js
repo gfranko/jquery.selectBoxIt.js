0d146a61f5237e76761bca92f711aa87c24626de
Greg Franko <gregfranko5@gmail.com>
2012-10-31 02:07:00 -0400

    Adding new bootstrap buttons demo page

--- src/javascripts/jquery.selectBoxIt.js
+++ src/javascripts/jquery.selectBoxIt.js
@@ -1,4 +1,4 @@
-/* jquery Selectboxit - v1.8.0 - 2012-09-28
+/* jquery Selectboxit - v1.9.0 - 2012-10-31
 * http://www.gregfranko.com/jQuery.selectBoxIt.js/
 * Copyright (c) 2012 Greg Franko; Licensed MIT */
 
@@ -26,7 +26,7 @@
 
         // Plugin version
 
-        VERSION: "1.8.0",
+        VERSION: "1.9.0",
 
         // These options will be used as defaults
         options: {
@@ -75,7 +75,9 @@
 
                 return (/android.+mobile|avantgo|bada\/|blackberry|ipad|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(ua)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i.test(ua.substr(0,4)));
 
-            }
+            },
+
+            nostyle: false
 
         },
 
@@ -439,10 +441,7 @@
                 "class": "selectboxit-arrow-container",
 
                 // IE specific attribute to not allow the dropdown list text to be selected
-                "unselectable": "on",
-
-                // The dynamic CSS of the down arrow container element
-                "style": "height:" + height + "px;"
+                "unselectable": "on"
 
             }).
 
@@ -450,21 +449,30 @@
             append(self.downArrow);
 
             // Appends the down arrow element to the dropdown list
-            self.div.append(self.downArrowContainer);
+            self.div.append(this.options.nostyle ? self.downArrow : self.downArrowContainer);
 
-            // Dynamically adds the `max-width` and `line-height` CSS styles of the dropdown list text element
-            self.divText.css({
+            if (!self.options.nostyle) {
 
-                "line-height": self.div.css("height"),
+                // The dynamic CSS of the down arrow container element
+                self.downArrowContainer.css({
+                    "height": height + "px"
+                });
 
-                "max-width": self.div.width() - (self.downArrowContainer.width() + self.divImage.width())
-            });
+                // Dynamically adds the `max-width` and `line-height` CSS styles of the dropdown list text element
+                self.divText.css({
 
-            self.divImage.css({
+                    "line-height": self.div.css("height"),
 
-                "margin-top": height / 4
+                    "max-width": self.div.width() - (self.downArrowContainer.width() + self.divImage.width())
+                });
 
-            });
+                self.divImage.css({
+
+                    "margin-top": height / 4
+
+                });
+
+            }
 
             // Maintains chainability
             return self;
@@ -1267,17 +1275,21 @@
 
             });
 
-            if(self.options.theme === "twitterbootstrap" && arrowClass === "caret") {
+            if (!self.options.nostyle) {
 
-                // Adds the jqueryUI down arrow icon CSS class to the down arrow div
-                self.downArrow.css({ "margin-top": self.downArrowContainer.height()/2 });
+                if(self.options.theme === "twitterbootstrap" && arrowClass === "caret") {
 
-            }
+                    // Adds the jqueryUI down arrow icon CSS class to the down arrow div
+                    self.downArrow.css({ "margin-top": self.downArrowContainer.height()/2 });
 
-            else {
+                }
 
-                // Adds the jqueryUI down arrow icon CSS class to the down arrow div
-                self.downArrow.css({ "margin-top": self.downArrowContainer.height()/4 });
+                else {
+
+                    // Adds the jqueryUI down arrow icon CSS class to the down arrow div
+                    self.downArrow.css({ "margin-top": self.downArrowContainer.height()/4 });
+
+                }
 
             }
 
@@ -1518,7 +1530,10 @@ $(function() {
             attr("disabled", "disabled");
 
             //Makes the dropdown list not focusable by removing the `tabindex` attribute
-            self.div.removeAttr("tabindex").css("cursor", "default");
+            self.div.removeAttr("tabindex")
+
+            //Enabled styling for disabled state
+            .addClass("selectboxit-disabled");
 
             // Calls the jQueryUI Widget Factory disable method to make sure all options are correctly synced
             $.Widget.prototype.disable.call(self);
@@ -1631,8 +1646,11 @@ $(function() {
             removeAttr("disabled");
 
             // Make the dropdown list focusable
-            self.div.attr("tabindex", 0).css("cursor", "pointer");
+            self.div.attr("tabindex", 0)
 
+            // Disable styling for disabled state
+            .removeClass("selectboxit-disabled");
+            
             $.Widget.prototype.enable.call(self);
 
             // Provide callback function support

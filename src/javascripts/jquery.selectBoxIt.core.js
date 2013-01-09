be25693ebfd5e94a6040397e9c2849c994bcbe21
Greg Franko <gregfranko5@gmail.com>
2013-01-09 13:59:28 -0500

    Added mouseup event support

--- src/javascripts/jquery.selectBoxIt.core.js
+++ src/javascripts/jquery.selectBoxIt.core.js
@@ -857,6 +857,13 @@
 
                 },
 
+                // `mouseup` event with the `selectBoxIt` namespace
+                "mouseup.selectBoxIt": function() {
+
+                    self.triggerEvent("mouseup");
+
+                },
+
                 // `blur` event with the `selectBoxIt` namespace.  Uses special blur logic to make sure the dropdown list closes correctly
                 "blur.selectBoxIt": function() {
 

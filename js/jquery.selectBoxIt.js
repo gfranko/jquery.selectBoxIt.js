e1e9df890c1d48f3b52504edd44a531d90f26990
Greg Franko <gregfranko5@gmail.com>
2013-01-24 23:59:38 -0500

    Updated SelectBoxIt

--- js/jquery.selectBoxIt.js
+++ js/jquery.selectBoxIt.js
@@ -25,7 +25,6 @@
     $.widget("selectBox.selectBoxIt", {
 
         // Plugin version
-
         VERSION: "2.9.9",
 
         // These options will be used as defaults
@@ -286,7 +285,9 @@
                 "tabindex": self.selectBox.attr("tabindex") || "0",
 
                 // IE specific attribute to not allow the element to be selected
-                "unselectable": "on"
+                "unselectable": "on",
+
+                "data": $.extend({}, true, {}, self.selectBox.data())
 
             }).
 
